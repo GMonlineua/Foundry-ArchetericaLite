0b70c2d557288a0ed4424af914aa5ee0334c99de
GMonline <gmonineua@gmail.com>
2023-01-26 19:30:06 +0200

    fix localize and npc sheet

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -199,7 +199,7 @@ export class ArchetericaLiteActorSheet extends ActorSheet {
     // Grab any data associated with this control.
     const data = duplicate(header.dataset);
     // Initialize a default name.
-    const name = `New ${type.capitalize()}`;
+    const name = game.i18n.localize("ARCHETERICALITE.NewItem") + " " + game.i18n.localize(`ITEM.Type${type.capitalize()}`);
     // Prepare the item object.
     const itemData = {
       name: name,
