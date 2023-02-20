d8ea5ca943636699d5fbb1e52dafbab6072541a6
GMonline <gmonineua@gmail.com>
2023-02-20 16:27:34 +0200

    version 0.4

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -36,13 +36,7 @@ export class ArchetericaLiteActorSheet extends ActorSheet {
 
   /** @override */
   getData() {
-    // Retrieve the data structure from the base sheet. You can inspect or log
-    // the context variable to see the structure, but some key properties for
-    // sheets are the actor object, the data object, whether or not it's
-    // editable, the items array, and the effects array.
     const context = super.getData();
-
-    // Use a safe clone of the actor data for further operations.
     const actorData = this.actor.toObject(false);
 
     // Add the actor's data to context.data for easier access, as well as flags.
