4237c26a0692f289d44467104ebf77e9c17d1f79
GMonline <gmonineua@gmail.com>
2023-07-05 15:59:09 +0300

    roll update

--- module/helpers/roll.mjs
+++ module/helpers/roll.mjs
@@ -1,259 +1,203 @@
 export async function prepareRollDialog(sheet, type) {
-  const speaker = ChatMessage.getSpeaker({ actor: sheet.actor });
-
-  if (type === "narrative") {
-    const testName = game.i18n.localize("ARCHETERICALITE.NarrativeTest");
-    const template_data = { narrative: true, modifier: true };
-    const rendered_html = await renderHtml(template_data);
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => narrativeTest(testName, speaker, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "embarrassment") {
-    const testName = game.i18n.localize("ARCHETERICALITE.EmbarrassmentTest");
-    const template_data = { modifier: true };
-    const rendered_html = await renderHtml(template_data);
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => embarrassment(testName, speaker, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "brawl") {
-    const testName = game.i18n.localize("ARCHETERICALITE.MasteryTest") + ": " + game.i18n.localize("ARCHETERICALITE.Brawl");
-    const template_data = { mastery: true };
-    const rendered_html = await renderHtml(template_data);
-    let actorMastery = sheet.object.system.combat.brawl;
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => masteryTest(testName, speaker, actorMastery, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "fencing") {
-    const testName = game.i18n.localize("ARCHETERICALITE.MasteryTest") + ": " + game.i18n.localize("ARCHETERICALITE.Fencing");
-    const template_data = { mastery: true };
-    const rendered_html = await renderHtml(template_data);
-    let actorMastery = sheet.object.system.combat.fencing;
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => masteryTest(testName, speaker, actorMastery, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "firearms") {
-    const testName = game.i18n.localize("ARCHETERICALITE.MasteryTest") + ": " + game.i18n.localize("ARCHETERICALITE.Firearms");
-    const template_data = { mastery: true };
-    const rendered_html = await renderHtml(template_data);
-    let actorMastery = sheet.object.system.combat.firearms;
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => masteryTest(testName, speaker, actorMastery, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "throwing") {
-    const testName = game.i18n.localize("ARCHETERICALITE.MasteryTest") + ": " + game.i18n.localize("ARCHETERICALITE.Throwing");
-    const template_data = { mastery: true };
-    const rendered_html = await renderHtml(template_data);
-    let actorMastery = sheet.object.system.combat.throwing;
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => masteryTest(testName, speaker, actorMastery, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "damage") {
-    const testName = game.i18n.localize("ARCHETERICALITE.Damage");
-    const template_data = { damage: true };
-    const rendered_html = await renderHtml(template_data);
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => damageTest(testName, speaker, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else if (type === "imago") {
-    const testName = game.i18n.localize("ARCHETERICALITE.ImagoTest");
-    const template_data = { imago: true, modifier: true };
-    const rendered_html = await renderHtml(template_data);
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => imagoTest(testName, speaker, sheet, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
-
-  else {
-    const testName = game.i18n.localize("ARCHETERICALITE.StandartTest");
-    const template_data = { customDifficulty: true, modifier: true };
-    const rendered_html = await renderHtml(template_data);
-    let submit = {
-      icon: '<i class="fas fa-check"></i>',
-      label: game.i18n.localize("ARCHETERICALITE.Roll"),
-      callback: (html) => standartTest(testName, speaker, html)
-    };
-    createDialog(testName, rendered_html, submit);
-  }
+  const { testName, templateData, testFunction, actorMastery } = getTestData(type, sheet);
+  await createDialogAndSubmit(testName, templateData, sheet, testFunction, actorMastery);
 }
 
-function renderHtml(template_data) {
-  const template_file = "systems/archetericalite/templates/apps/rollTest.hbs";
-  const rendered_html = renderTemplate(template_file, template_data);
-  return rendered_html;
+function getTestData(type, sheet) {
+  const testData = {
+    narrative: {
+      testName: game.i18n.localize("ARCHETERICALITE.NarrativeTest"),
+      templateData: { narrative: true, modifier: true },
+      testFunction: narrativeTest,
+    },
+    embarrassment: {
+      testName: game.i18n.localize("ARCHETERICALITE.EmbarrassmentTest"),
+      templateData: { modifier: true },
+      testFunction: embarrassment,
+    },
+    brawl: {
+      testName: `${game.i18n.localize("ARCHETERICALITE.MasteryTest")}: ${game.i18n.localize(
+        "ARCHETERICALITE.Brawl"
+      )}`,
+      templateData: { mastery: true },
+      testFunction: masteryTest,
+      actorMastery: sheet.object.system.combat.brawl,
+    },
+    fencing: {
+      testName: `${game.i18n.localize("ARCHETERICALITE.MasteryTest")}: ${game.i18n.localize(
+        "ARCHETERICALITE.Fencing"
+      )}`,
+      templateData: { mastery: true },
+      testFunction: masteryTest,
+      actorMastery: sheet.object.system.combat.fencing,
+    },
+    firearms: {
+      testName: `${game.i18n.localize("ARCHETERICALITE.MasteryTest")}: ${game.i18n.localize(
+        "ARCHETERICALITE.Firearms"
+      )}`,
+      templateData: { mastery: true },
+      testFunction: masteryTest,
+      actorMastery: sheet.object.system.combat.firearms,
+    },
+    throwing: {
+      testName: `${game.i18n.localize("ARCHETERICALITE.MasteryTest")}: ${game.i18n.localize(
+        "ARCHETERICALITE.Throwing"
+      )}`,
+      templateData: { mastery: true },
+      testFunction: masteryTest,
+      actorMastery: sheet.object.system.combat.throwing,
+    },
+    damage: {
+      testName: game.i18n.localize("ARCHETERICALITE.Damage"),
+      templateData: { damage: true },
+      testFunction: damageTest,
+    },
+    imago: {
+      testName: game.i18n.localize("ARCHETERICALITE.ImagoTest"),
+      templateData: { imago: true, modifier: true },
+      testFunction: imagoTest,
+    },
+    default: {
+      testName: game.i18n.localize("ARCHETERICALITE.StandartTest"),
+      templateData: { customDifficulty: true, modifier: true },
+      testFunction: standartTest,
+    },
+  };
+
+  return testData[type] || testData.default;
 }
 
-function createDialog(testName, rendered_html, submit) {
-  let d = new Dialog(
-    {
-      title: testName,
-      content: rendered_html,
-      buttons: {
-        roll: submit,
-        cancel: {
-          icon: '<i class="fas fa-times"></i>',
-          label: game.i18n.localize("ARCHETERICALITE.RollCancel"),
-          callback: () => {},
+async function createDialogAndSubmit(testName, templateData, sheet, testFunction, actorMastery) {
+  const html = await renderTemplate("systems/archetericalite/templates/apps/rollDialog.hbs", templateData);
+  const dialog = new Dialog({
+    title: testName,
+    content: html,
+    buttons: {
+      roll: {
+        label: game.i18n.localize("ARCHETERICALITE.Roll"),
+        icon: '<i class="fas fa-dice"></i>',
+        callback: async (html) => {
+          try {
+            const formData = new FormData(html[0].querySelector("form"));
+            const data = Object.fromEntries(formData.entries());
+
+            const speaker = ChatMessage.getSpeaker({ actor: sheet.actor });
+            await testFunction(testName, speaker, data, sheet, actorMastery);
+          } catch (error) {
+            console.error("Error submit in roll dialog:", error);
+          }
         },
       },
-      default: "roll",
-      close: () => {},
+      cancel: {
+        icon: '<i class="fas fa-times"></i>',
+        label: game.i18n.localize("ARCHETERICALITE.RollCancel"),
+        callback: () => {},
+      },
     },
-    { width: "300" }
-  );
-  d.render(true);
+    default: "roll",
+    close: () => {},
+  });
+  dialog.render(true);
 }
 
-function narrativeTest(testName, speaker, sheet, html) {
-  let infirmity = sheet.object.system.infirmity;
-  let stress = sheet.object.system.stress;
-  let difficulty = parseInt(html.find("#narrativeDifficulty")[0].value, 10);
-  let modifier = parseInt(html.find("#modifier")[0].value, 10);
-  if (infirmity > 0) {
-    infirmityRoll(testName, speaker, modifier, infirmity)
-  }
-  else {
-    rollDice(testName, speaker, modifier, difficulty, stress)
-  }
+function narrativeTest(testName, speaker, data, sheet) {
+  const infirmity = sheet.object.system.infirmity;
+  const stress = sheet.object.system.stress;
+  const difficulty = data.narrativeDifficulty;
+  const modifier = data.modifier;
+  rollDice(testName, speaker, modifier, difficulty, stress, infirmity);
 }
 
-function embarrassment(testName, speaker, sheet, html) {
-  let difficulty = sheet.object.system.stress;
-  let modifier = parseInt(html.find("#modifier")[0].value, 10);
-  rollDice(testName, speaker, modifier, difficulty)
-}
-
-function masteryTest(testName, speaker, actorMastery, sheet, html) {
-  let infirmity = sheet.object.system.infirmity;
-  let modifier = html.find("#advantage")[0].value;
-  let opponentMastery = parseInt(html.find("#opponentMastery")[0].value, 10);
-  let difficulty = 7 - actorMastery + opponentMastery;
-  if (infirmity > 0) {
-    infirmityRoll(testName, speaker, modifier, infirmity)
-  }
-  else {
-    rollDice(testName, speaker, modifier, difficulty)
-  }
-}
-
-function imagoTest(testName, speaker, sheet, html) {
-  let modifier = parseInt(html.find("#modifier")[0].value, 10);
-  let opponentImago = parseInt(html.find("#imago")[0].value, 10);
-  let actorImago = sheet.object.system.mystical.imago;
-  let difficulty = 7 - actorImago + opponentImago;
-  rollDice(testName, speaker, modifier, difficulty);
-
-}
-
-function standartTest(testName, speaker, html) {
-  let modifier = parseInt(html.find("#modifier")[0].value, 10);
-  let difficulty = parseInt(html.find("#customDifficulty")[0].value, 10);
+function embarrassment(testName, speaker, data, sheet) {
+  const difficulty = sheet.object.system.stress;
+  const modifier = data.modifier;
   rollDice(testName, speaker, modifier, difficulty);
 }
 
-async function rollDice(testName, speaker, modifier, difficulty, stress) {
-  let roll = new Roll("2d6+@modifier", {modifier});
-  await roll.evaluate({ async: true });
-  let dice = [ roll.terms[0].results[0].result, roll.terms[0].results[1].result ]
-  let total = roll.total;
-  let success = (total >= difficulty);
-  let data = { testName, modifier, dice, total, difficulty, success };
-  if ( stress >= 7 && stress >= total) {
-    data.panic = true
-  };
-  let chatMessage = await renderTemplate("systems/archetericalite/templates/apps/rollResult.hbs", data);
-  roll.toMessage({
-    speaker: speaker,
-    content: chatMessage
-  });
+function masteryTest(testName, speaker, data, sheet, actorMastery) {
+  const infirmity = sheet.object.system.infirmity;
+  const modifier = data.advantage;
+  const opponentMastery = parseInt(data.opponentMastery, 10);
+  const difficulty = 7 - actorMastery + opponentMastery;
+  rollDice(testName, speaker, modifier, difficulty, null, infirmity);
 }
 
-async function infirmityRoll(testName, speaker, modifier, infirmity) {
-  let roll = new Roll("2d6+@modifier", {modifier});
-  let infirmityResult = new Roll(infirmity + "d6");
-  await roll.evaluate({ async: true });
-  await infirmityResult.evaluate({ async: true });
+function imagoTest(testName, speaker, data, sheet) {
+  const modifier = data.modifier;
+  const opponentImago = parseInt(data.opponentImago, 10);
+  const actorImago = sheet.object.system.mystical.imago;
+  const difficulty = 7 - actorImago + opponentImago;
+  rollDice(testName, speaker, modifier, difficulty);
+}
 
-  let dice = [ roll.terms[0].results[0].result, roll.terms[0].results[1].result ];
-  let infirmityDice = [];
-  for (const element of infirmityResult.terms[0].results) {
-    infirmityDice.push(element.result);
+function standartTest(testName, speaker, data) {
+  const modifier = data.modifier;
+  const difficulty = data.customDifficulty;
+  rollDice(testName, speaker, modifier, difficulty);
+}
+
+async function rollDice(testName, speaker, modifier, difficulty, stress, infirmity) {
+  try {
+    const roll = new Roll("2d6+@modifier", { modifier });
+    const infirmityRoll = infirmity ? new Roll(`${infirmity}d6`) : null;
+    await Promise.all([roll.evaluate({ async: true }), infirmityRoll?.evaluate({ async: true })]);
+
+    const dice = roll.terms[0].results.map((result) => result.result);
+    const infirmityDice = infirmityRoll ? infirmityRoll.terms[0].results.map((result) => result.result) : null;
+
+    const excludedValues = infirmityDice ? new Set(infirmityDice) : null;
+    const total = dice.reduce((sum, value) => {
+      if (excludedValues && excludedValues.has(value)) {
+        return sum;
+      }
+      return sum + value;
+    }, 0);
+
+    const fainting = total === 0;
+    const success = total >= difficulty;
+    const panic = stress >= 7 && stress > total;
+
+    const renderData = { testName, modifier, dice, infirmityDice, total, difficulty, success, panic, fainting };
+    await renderAndSendRollResult(speaker, renderData);
+  } catch (error) {
+    console.error("Error performing roll:", error);
   }
-
-  let total = "?";
-  let data = { testName, modifier, dice, infirmityDice, total };
-  let chatMessage = await renderTemplate("systems/archetericalite/templates/apps/rollResult.hbs", data);
-  roll.toMessage({
-    speaker: speaker,
-    content: chatMessage
-  });
 }
 
-async function damageTest(testName, speaker, html) {
-  let damageHtml = html.find("#damage")[0];
-  let damage = damageHtml.value;
-  let modifier = parseInt(html.find("#lethality")[0].value, 10);
-  testName = damageHtml.options[damageHtml.selectedIndex].text;
 
-  if (damage != "2")
-  {
-    let roll = new Roll("@damage+@modifier", {damage, modifier});
-    await roll.evaluate({ async: true });
-    let dice = [ roll.dice[0].results[0].result, roll.dice[0].results[1].result ];
-    let total = roll.total;
-    let data = { testName, modifier, dice, total };
-    let chatMessage = await renderTemplate("systems/archetericalite/templates/apps/rollResult.hbs", data);
+async function damageTest(testName, speaker, data) {
+  try {
+    const damage = data.damage;
+    const modifier = parseInt(data.modifier, 10);
 
-    roll.toMessage({
+    if (damage !== "2") {
+      const roll = new Roll("@damage+@modifier", { damage, modifier });
+      await roll.evaluate({ async: true });
+      const dice = [roll.dice[0].results[0].result, roll.dice[0].results[1].result];
+      const total = roll.total;
+      const renderData = { testName, modifier, dice, total };
+      await renderAndSendRollResult(speaker, renderData, roll);
+    } else {
+      const total = 2 + modifier;
+      const renderData = { testName, modifier, total };
+      await renderAndSendRollResult(speaker, renderData);
+    }
+  } catch (error) {
+    console.error("Error in damageTest:", error);
+  }
+}
+
+async function renderAndSendRollResult(speaker, renderData, roll) {
+  try {
+    const chatMessage = await renderTemplate("systems/archetericalite/templates/apps/rollResult.hbs", renderData);
+    const chatData = {
       speaker: speaker,
-      content: chatMessage
-    });
-  }
-  else {
-    let total = 2 + modifier;
-    let data = { testName, modifier, total };
-    let chatMessage = await renderTemplate("systems/archetericalite/templates/apps/rollResult.hbs", data);
-    let chatData = {
-        speaker: speaker,
-        content: chatMessage,
+      content: chatMessage,
+      sound: CONFIG.sounds.dice,
+      isRoll: true,
     };
-    ChatMessage.create(chatData, {});
+    ChatMessage.create(chatData);
+  } catch (error) {
+    console.error("Error rendering and sending roll result:", error);
   }
 }
